176bebc119f0b6213558dda61862c31bf19426e6
Enrico Scherlies <e.scherlies@me.com>
2017-10-25 12:49:01 +0200

    del

--- fixtures/enrico.js
+++
@@ -1,8 +0,0 @@
-let enrico = {"cWaGTqcy5eTeLADJKZMCWtzmL9N2" : {
-  "daycares" : {
-    "-KjZguyFWdyWZhS3Apua" : true
-  },
-  "firstname" : "Enrico",
-  "lastname" : "Scherlies",
-  "fullname" : "Enrico Scherlies"
-}}
